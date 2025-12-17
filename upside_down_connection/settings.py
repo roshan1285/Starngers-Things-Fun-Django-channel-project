02ac56077e4f56034048bd45d57e729651f62217
roshan1285 <roshansavani85@gmail.com>
2025-12-17 14:08:49 +0530

    Update font source in base template and adjust landing page styles for improved aesthetics

--- upside_down_connection/settings.py
+++ upside_down_connection/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'login'
+LOGIN_URL = 'landing'
 LOGIN_REDIRECT_URL = 'home'
 
 STATIC_URL = 'static/'
